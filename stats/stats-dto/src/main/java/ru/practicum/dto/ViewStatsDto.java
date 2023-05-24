8c6ad001b14e2bd0efc57aae705b54548577a6bd
Kirill.Babichenko <logiant@gmail.com>
2023-05-25 01:24:53 +0300

    feat: Реализовать основной функционал приложения в main-service. Настроить взаимодействие main-service с сервисом статистики.

--- stats/stats-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
@@ -3,10 +3,9 @@ package ru.practicum.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 @Data
-@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
